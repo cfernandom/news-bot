8fd79c3ee877e0cd446574297e9a3dcc312e1fcd
Cristhian Fernando Moreno Manrique <92770549+cfernandom@users.noreply.github.com>
2025-05-12 08:36:48 -0500

    omitir scraper mayoclinic
    el summary de este scraper es muy ambiguo y puede confundir al LLM

--- services/scraper/src/main.py
+++ services/scraper/src/main.py
@@ -1,7 +1,6 @@
 from services.scraper.src.extractors.www_breastcancer_org import scraper__www_breast_cancer_org
 from services.scraper.src.extractors.www_breastcancernow_org import scraper__breast_cancer_now_org
 from services.scraper.src.extractors.www_breastcancernews_features import scraper__webmd_breast_cancer
-from services.scraper.src.extractors.www_newsnetwork_mayoclinic_org import scraper__mayoclinic
 from services.scraper.src.extractors.www_scraper_sciencedaily import scraper__sciencedaily
 from services.scraper.src.extractors.www_scraper__news_medical import scraper__news_medical
 from services.shared.models.article import Article
@@ -12,8 +11,7 @@ async def scrape_articles() -> list[Article]:
     article1 = await scraper__www_breast_cancer_org()
     article2 = await scraper__breast_cancer_now_org()
     article3 = await scraper__webmd_breast_cancer()
-    article4 = await scraper__mayoclinic()
     article5 = await scraper__sciencedaily()
     article6 = await scraper__news_medical()
-    articles.extend(article1 + article2 + article3 + article4 + article5 + article6)
+    articles.extend(article1 + article2 + article3 + article5 + article6)
     return articles
