789febf7f9712df4d90c3505b0750c50d330fb7d
cfernandom <cristian_21_97@hotmail.com>
2025-06-02 17:02:09 -0500

    feat(scrapers): add retry mechanism for HTML page loading in get_html_with_playwright

--- services/scraper/src/utils.py
+++ services/scraper/src/utils.py
@@ -4,7 +4,16 @@ async def get_html_with_playwright(url):
     async with async_playwright() as p:
         browser = await p.chromium.launch()
         page = await browser.new_page()
-        await page.goto(url)
+        try:
+            await page.goto(url)
+        except Exception as e:
+            print(f"Error al cargar la página {url}. Reintentando...")
+            try:
+                await page.goto(url, timeout=60000)
+            except Exception as e:
+                print(f"Error al cargar la página {url} después del reintento: {e}")
+                await browser.close()
+                return None
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
         html = await page.content()
         await browser.close()
